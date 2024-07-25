de6586b4c7ab8b631893bc180d4ec9cf68c88977
telor 3 <105234460+kemillyluan@users.noreply.github.com>
2024-07-25 11:28:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultados");
+const textoresultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternatvas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+{
+        enunciado: "Pergunta 2",
+        alternatvas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+
+];
